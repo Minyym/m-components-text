70b80780c63b8350191c8aecbfd3bfda8221a733
yuyanmin <1812006192@qq.com>
2022-03-24 11:04:10 +0800

    feat: 1.0.1

---
+++ index.tsx
@@ -0,0 +1,4 @@
+import Button from "./src/components/Button/Button";
+import { coverDigit2Uppercase } from "./src/util";
+
+export { Button, coverDigit2Uppercase };
--- src/index.tsx
+++
@@ -1,4 +0,0 @@
-import Button from "./components/Button/Button";
-export * from "./util";
-
-export { Button };
